0d50bd591343392849dcc2bf149ed222bc457409
Tin Hoang Nguyen <kenvin.alone@gmail.com>
2014-12-12 10:58:48 +0700

    I up load file

---
+++ js/chenflash.js
@@ -0,0 +1,7 @@
+document.write("<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' codebase='http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0' width='100%' height='540'>");
+document.write("<param name='movie' value='http://jatbirat.info/event/noel/images/snow.swf'/>");
+document.write("<param name='quality' value='high' />");
+document.write("<param name='wmode' value='transparent'/>");
+document.write("<embed src='http://jatbirat.info/event/noel/images/snow.swf' wmode='transparent'quality ='high' pluginspage='http://www.macromedia.com/go/getflashplayer' type='application/x-shockwave-flash' width='100%' height='540'>");
+document.write("</embed>");
+document.write("</object>");
